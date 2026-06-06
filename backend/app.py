dc2dc369fae1e7c59d60082b4a6aaa970ee1315d
AshishMittal345 <ashishmittal192@gmail.com>
2026-06-06 15:29:28 +0530

    Create backend folder

---
+++ backend/app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from groq import Groq
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+app = FastAPI()
+
+class ChatRequest(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"message": "AI Persona Backend Working"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": request.message
+            }
+        ]
+    )
+
+    answer = response.choices[0].message.content
+
+    return {
+        "reply": answer
+    }
